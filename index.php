cf30c94c045b33d7c2878d2cb8479e21d8ff91dd
giagra91 <giagra91@gmail.com>
2022-04-19 14:44:59 +0200

    modifiche su strlen per lunghezza paragrafo

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
     <title>Badwords php</title>
 </head>
 <body>
+    <pre>Prova</pre>
     <p> <?php echo str_replace($word, "***", $paragraph) ?> </p>
-    <p>La lunghezza del paragrafo è di <?php echo strlen($paragraph) ?> tra caratteri e spazi.</p>
+    <p>La lunghezza del paragrafo è di <?php echo strlen(str_replace($word, "***", $paragraph)) ?> tra caratteri e spazi.</p>
     <p>La parola sostituita è "<?php echo $word ?>".</p>
 </body>
 </html>
